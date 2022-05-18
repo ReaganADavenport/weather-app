b1b4a61ff7b16d8905602117a4266fd5e12afcfd
Reagan Davenport <reagan.a.davenport@gmail.com>
2022-05-18 14:57:54 -0400

    added styles to the search bar, added date, temp, and weather to App.js

--- src/App.js
+++ src/App.js
@@ -11,6 +11,20 @@ const api = {
 
 
 function App() {
+
+  const dateBuilder = (d) => {
+    let months = ["January", "February", "March", "April", "May", "June", "July", "August",
+      "September", "October", "November", "December"];
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+
+    return `${day} ${date} ${month} ${year}`
+  }
+
   return(
     <div className="App">
       <main>
@@ -20,7 +34,18 @@ function App() {
             className='search-bar'
             placeholder='Search...'
           />
-          
+        </div>
+        <div className='location-box'>
+          <div className='location'>
+            Atlanta, US
+          </div>
+          <div className='date'>{dateBuilder(new Date())}</div>
+        </div>
+        <div className='weather-box'>
+          <div className='temp'>
+            80˚f
+          </div>
+          <div className='weather'>Sunny</div>
         </div>
       </main>
     </div>
